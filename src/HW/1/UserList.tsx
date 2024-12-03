b58b22649faa61c2b8c15170665dfb93c0ac20cd
ViacheslavKh1654 <viacheslav.fictional.user@gmail.com>
2024-12-03 22:55:21 +0100

    c

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -16,12 +16,12 @@ type UserListPropsType = {
 
 export const UserList = (props: UserListPropsType) => {
   return (
-    <div id={'hw01-users'}>
+    <div id={'#hw01-users'}>
       <h2>User List:</h2>
 
       <ul>
         {props.users.map((user) => (
-          <li key={user.id} id={`hw01-user-${user.id}`}>
+          <li key={user.id} id={`#hw01-user-${user.id}`}>
             <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
             {user.address.street}, {user.address.city}
           </li>
